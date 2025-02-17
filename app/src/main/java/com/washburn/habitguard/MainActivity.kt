7ecdbda3b9781e925b735f4e692923837b3390f9
Viral3651 <jvillanueva3651@gmail.com>
2025-02-17 10:34:33 -0600

    Added sign out button to MainActivity to redirect back to AuthActivity.

--- app/src/main/java/com/washburn/habitguard/MainActivity.kt
+++ app/src/main/java/com/washburn/habitguard/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.washburn.habitguard.ui.theme.HabitGuardTheme
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -27,6 +28,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         auth = Firebase.auth
+
+        val signOutButton = findViewById<Button>(R.id.signOutButton)
+        signOutButton.setOnClickListener {
+            // Sign out the user
+            auth.signOut()
+
+            // Redirect to AuthActivity
+            startActivity(Intent(this, AuthActivity::class.java))
+            finish()  // finish() so user can't come back by pressing back
+        }
     }
 
     override fun onStart() {
