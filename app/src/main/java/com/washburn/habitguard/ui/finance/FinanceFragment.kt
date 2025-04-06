2d36651694f89c625e026ce7ca6085b0fa98c297
Viral3651 <jvillanueva3651@gmail.com>
2025-04-05 21:20:52 -0500

    Changed a FinanceFragment.kt import to habitguard Transaction data class rather than firebase transaction.

--- app/src/main/java/com/washburn/habitguard/ui/finance/FinanceFragment.kt
+++ app/src/main/java/com/washburn/habitguard/ui/finance/FinanceFragment.kt
@@ -6,7 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import com.google.firebase.firestore.Transaction
+import com.washburn.habitguard.ui.finance.Transaction
 import com.washburn.habitguard.databinding.FragmentFinanceBinding
 
 class FinanceFragment : Fragment() {
